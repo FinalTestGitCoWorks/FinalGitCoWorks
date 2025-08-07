f86ae086194f41543315cc143113d30e7773b889
Trajectory-SH <trajectory0611@gmail.com>
2025-08-07 14:34:16 +0900

    ✨ [#20]hello world sout 작성

--- backend/demo/src/main/java/git_test/login/Login.java
+++ backend/demo/src/main/java/git_test/login/Login.java
@@ -2,6 +2,7 @@ package git_test.login;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println("hello world!!!");
+        System.out.println("hello world!!");
+
     }
 }
